0327d5301ef0c78e3d07af5f918a1e93a798585e
Christian Tischer <christian.tischer@embl.de>
2018-05-18 11:18:20 +0200

    refactor prospr

---
+++ src/main/java/de/embl/cba/registration/prospr/ProSPrRegistration.java
@@ -0,0 +1,108 @@
+package de.embl.cba.registration.prospr;
+
+import mpicbg.spim.data.SpimData;
+import mpicbg.spim.data.registration.ViewRegistration;
+import mpicbg.spim.data.registration.ViewTransform;
+import mpicbg.spim.data.registration.ViewTransformAffine;
+import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imglib2.FinalRealInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.realtransform.AffineTransform3D;
+
+public class ProSPrRegistration
+{
+    public static AffineTransform3D getTransformJRotation( SpimData emData )
+    {
+
+        AffineTransform3D transformJTransform = new AffineTransform3D(  );
+
+        transformJTransform.set(
+                -0.6427876097,   0.7660444431,   0.0,   0.0,
+                0.0, 0.0, 1.0, 0.0,
+                0.7660444431,  0.6427876097,  0.0, 0.0 );
+
+
+        FinalRealInterval boundingIntervalAfterTransformation = getBoundsAfterRotation( emData, transformJTransform );
+
+        double[] transformJTranslation = new double[]{
+                - boundingIntervalAfterTransformation.realMin( 0 ),
+                - boundingIntervalAfterTransformation.realMin( 1 ) ,
+                - boundingIntervalAfterTransformation.realMin( 2 ) };
+
+        transformJTransform.translate( transformJTranslation );
+
+        return transformJTransform;
+    }
+
+    public static AffineTransform3D getCombinedTransform( AffineTransform3D firstTransform, AffineTransform3D secondTransform )
+    {
+        AffineTransform3D combinedTransform = firstTransform.copy();
+        combinedTransform.preConcatenate( secondTransform );
+        return combinedTransform;
+    }
+
+    public static FinalRealInterval getBoundsAfterRotation( SpimData emData, AffineTransform3D transformJTransform )
+    {
+        RandomAccessibleInterval< ? > image = emData.getSequenceDescription().getImgLoader().getSetupImgLoader( 0 ).getImage( 0 );
+        return transformJTransform.estimateBounds( image );
+    }
+
+    public static double[] getTranslationInPixels( SpimData data, double[] translationInMicrometer )
+    {
+        VoxelDimensions voxelDimensions = getVoxelDimensions( data );
+
+        double[] translationInPixels = new double[3];
+
+        for( int d = 0; d < 3; ++d )
+        {
+            translationInPixels[ d ] = translationInMicrometer[ d ] / voxelDimensions.dimension( d );
+        }
+
+        return translationInPixels;
+    }
+
+    public static VoxelDimensions getVoxelDimensions( SpimData data )
+    {
+        VoxelDimensions voxelDimensions = data.getSequenceDescription().getViewDescription( 0, 0 ).getViewSetup().getVoxelSize();
+        return voxelDimensions;
+    }
+
+    public static void setEmSimilarityTransform( SpimData spimData )
+    {
+
+        AffineTransform3D transformJRotation = getTransformJRotation( spimData );
+
+        AffineTransform3D elastixSimilarityTransform = getEmElastixSimilarityTransform( spimData );
+
+        AffineTransform3D combinedTransform = getCombinedTransform( transformJRotation, elastixSimilarityTransform );
+
+        adaptViewRegistration( spimData, combinedTransform );
+
+    }
+
+    public static void adaptViewRegistration( SpimData emData, AffineTransform3D transform )
+    {
+        // the ViewRegistration in the file contains the scaling relative to 1 micrometer
+        ViewRegistration viewRegistration = emData.getViewRegistrations().getViewRegistration( 0, 0 );
+        ViewTransform viewTransform = new ViewTransformAffine( "transform",  transform );
+        viewRegistration.concatenateTransform( viewTransform );
+    }
+
+    public static AffineTransform3D getEmElastixSimilarityTransform( SpimData data )
+    {
+        AffineTransform3D elastixFixedToMoving = new AffineTransform3D();
+
+        elastixFixedToMoving.set(
+                1.16299445, -0.04662481, -0.02350539, 0.0,
+                0.05221191,  1.04386046,  0.51274918, 0.0,
+                0.00054074, -0.51328738,  1.04490107, 0.0 );
+
+        double[] translationInMicrometer = new double[]{ 76.46363039, -78.88425459, 233.36234024 };
+
+        double[] translationInPixels = getTranslationInPixels( data, translationInMicrometer );
+
+        elastixFixedToMoving.translate( translationInPixels );
+
+        return elastixFixedToMoving.inverse();
+    }
+}
--- src/main/java/de/embl/cba/registration/ui/ProSPr.java
+++ src/main/java/de/embl/cba/registration/ui/ProSPr.java
@@ -2,19 +2,15 @@ package de.embl.cba.registration.ui;
 
 import bdv.util.*;
 import bdv.viewer.Interpolation;
-import bdv.viewer.Source;
-import bdv.viewer.SourceAndConverter;
+import de.embl.cba.registration.prospr.ProSPrRegistration;
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.XmlIoSpimData;
-import mpicbg.spim.data.registration.ViewRegistration;
-import mpicbg.spim.data.registration.ViewTransform;
-import mpicbg.spim.data.registration.ViewTransformAffine;
+import net.imagej.Dataset;
 import net.imagej.ImageJ;
-import net.imglib2.FinalRealInterval;
-import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.realtransform.AffineTransform3D;
@@ -31,90 +27,87 @@ import org.scijava.widget.Button;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 @Plugin(type = Command.class, menuPath = "Plugins>Registration>EMBL>ProSPr", initializer = "init")
 public class ProSPr extends DynamicCommand implements Interactive
 {
-    public static final String GENE_FILE_SUFFIX = ".xml";
-    public static final double PROSPR_SCALING_IN_MICROMETER = 0.5;
-    public static final String EM_FILE_ID = "em";
+    private static final String DATA_SOURCE_SUFFIX = ".xml";
+    private static final double PROSPR_SCALING_IN_MICROMETER = 0.5;
+    private static final String EM_RAW_FILE_ID = "em-raw";
+    private static final String EM_SEGMENTED_FILE_ID = "em-segmented";
+    private static final String GENE_SELECTION_UI = "Genes";
+    private static final ARGBType defaultGeneColor = new ARGBType( ARGBType.rgba( 255, 0, 255, 255 ) );
 
     @Parameter
     public LogService logService;
 
-    ArrayList< String > genes;
-
-    SpimData emData;
-
     Bdv bdv;
 
-    Map< String, BdvSource > geneSourceMap;
-    Map< String, File > geneFileMap;
+    Map< String, DataSource > dataSourcesMap;
 
-    public static String GENE_SELECTION_UI = "Genes";
+    String emRawDataID;
 
-    private static ARGBType defaultGeneColor = new ARGBType( ARGBType.rgba( 255, 0, 255, 255 ) );
+    private class DataSource
+    {
+        public SpimData spimData;
+        public BdvSource bdvSource;
+        public File file;
+        public Integer maxLutValue;
+        public boolean isActive;
+        public ARGBType color;
+    }
 
 
-    private void init() throws SpimDataException
+    public void init()
     {
         //File directory = new File( IJ.getDirectory( "Select ProSPr directory" ) );
 
-        File directory = new File( "/Users/tischer/Documents/detlev-arendt-clem-registration--data" );
+        //File directory = new File( "/Users/tischer/Documents/detlev-arendt-clem-registration--data" );
 
-        initGenes( directory );
+        //File directory = new File( "/Volumes/cba/tischer/projects/detlev-arendt-clem-registration--data/data/em-raw/bdv" );
 
-        initEM( directory );
+        //File directory = new File( "/Volumes/arendt/EM_6dpf_segmentation/bigdataviewer" );
 
-        initBdv();
+        String dir = IJ.getDirectory( "Please choose ProSPr directory" );
+
+        File directory = new File( dir );
+
+        initDataSources( directory );
+
+        createSourceSelectionUI( );
+
+        //initEm( directory );
+
+        initBdvWithEmRawData( );
 
         addActionButtons();
 
     }
 
-    private void initBdv()
+    public void run()
     {
-        bdv.getBdvHandle().getViewerPanel().setInterpolation( Interpolation.NLINEAR );
-        //bdv.getBdvHandle().getViewerPanel().setCurrentViewerTransform( new AffineTransform3D() );
+
     }
+
+
 
     private void addActionButtons()
     {
-        addShowGeneButton();
-
-        addHideGeneButton();
-
-        addChangeColorButton();
-    }
-
-    private void initEM( File directory ) throws SpimDataException
-    {
-        openEmData( directory );
-
-        setEmDataSimilarityTransform( );
-
-        bdv = showWithBdv( emData );
-
+        addActionButton( "Show", "showGene" );
+        addActionButton( "Hide", "hideGene" );
+        addActionButton( "Color", "setGeneColor" );
+        addActionButton( "Brightness", "setGeneBrightness" );
+        addActionButton( "Show legend", "showLegend" );
 
     }
 
-    private void initGenes( File directory )
+
+    private void addActionButton( String buttonName, String callback)
     {
-        geneSourceMap = new TreeMap<>(  );
-
-        setGeneFileMap( directory );
-
-        createGeneSelectionUI( );
-    }
-
-    private void addShowGeneButton()
-    {
-        final MutableModuleItem< Button > button = addInput( "Show", Button.class );
-        button.setCallback( "showGene" );
+        final MutableModuleItem< Button > button = addInput( buttonName, Button.class );
+        button.setCallback(callback );
         button.setRequired( false );
     }
 
@@ -123,28 +116,103 @@ public class ProSPr extends DynamicCommand implements Interactive
         showGene( ( String ) this.getInput( GENE_SELECTION_UI ) );
     }
 
+    private void print( String text )
+    {
+        logService.info( text );
+        IJ.log( text );
+    }
+
+
+    private void showLegend()
+    {
+        print( "Currently shown genes: " );
+
+        for ( String gene : dataSourcesMap.keySet() )
+        {
+            if ( dataSourcesMap.get( gene ).isActive )
+            {
+                String color = dataSourcesMap.get( gene ).color.toString();
+                print( gene + ", color " + color );
+            }
+        }
+    }
+
     private void showGene( String gene ) throws SpimDataException
     {
 
-        if ( geneSourceMap.keySet().contains( gene ) )
+        if ( dataSourcesMap.get( gene ).bdvSource == null )
         {
-            geneSourceMap.get( gene ).setActive( true );
-        }
-        else
-        {
-            switch ( GENE_FILE_SUFFIX )
+            switch ( DATA_SOURCE_SUFFIX )
             {
-                case ".tif": addSourceFromTiffFile( gene ); break;
-                case ".xml": addSourceFromBdvFile( gene ); break;
-                default: logService.error( "Unsupported format: " + GENE_FILE_SUFFIX );
+                case ".tif":
+                    addSourceFromTiffFile( gene );
+                    break;
+                case ".xml":
+                    setSourceFromBdvFile( gene );
+                    break;
+                default:
+                    logService.error( "Unsupported format: " + DATA_SOURCE_SUFFIX );
             }
+        }
+
+        dataSourcesMap.get( gene ).bdvSource.setActive( true );
+        dataSourcesMap.get( gene ).isActive =  true ;
+
+    }
+
+    private void hideGene( )
+    {
+        final String gene = ( String ) this.getInput( GENE_SELECTION_UI );
+
+        if ( dataSourcesMap.get( gene ).bdvSource != null  )
+        {
+            dataSourcesMap.get( gene ).bdvSource.setActive( false );
+            dataSourcesMap.get( gene ).isActive = false;
+        }
+    }
+
+    private void setGeneColor( ) throws SpimDataException
+    {
+        final String gene = ( String ) this.getInput( GENE_SELECTION_UI );
+
+        showGene( gene );
+
+        Color color = JColorChooser.showDialog( null, "Select color for " + gene, null );
+
+        if ( color != null )
+        {
+            dataSourcesMap.get( gene ).bdvSource.setColor( getArgbType( color ) );
+            dataSourcesMap.get( gene ).color = getArgbType( color );
+
         }
 
     }
 
-    private void addSourceFromBdvFile( String gene ) throws SpimDataException
+    private void setGeneBrightness( ) throws SpimDataException
     {
-        SpimData geneData = new XmlIoSpimData().load( geneFileMap.get( gene ).toString() );
+        final String gene = ( String ) this.getInput( GENE_SELECTION_UI );
+
+        showGene( gene );
+
+        GenericDialog gd = new GenericDialog("LUT max value");
+        gd.addNumericField("LUT max value: ", dataSourcesMap.get( gene ).maxLutValue, 0 );
+        gd.showDialog();
+        if (gd.wasCanceled()) return;
+
+        int max  = (int) gd.getNextNumber();
+
+        dataSourcesMap.get( gene ).bdvSource.setDisplayRange( 0.0, max  );
+        dataSourcesMap.get( gene ).maxLutValue = max;
+
+    }
+
+
+    private void setSourceFromBdvFile( String dataSourceName )
+    {
+
+        DataSource dataSource = dataSourcesMap.get( dataSourceName );
+
+        SpimData geneData = openSpimData( dataSource.file  ) ;
 
         geneData.getSequenceDescription()
                 .getViewDescription( 0,0  )
@@ -155,228 +223,121 @@ public class ProSPr extends DynamicCommand implements Interactive
                 .getChannel().setName( "AAA" );
 
         BdvSource source = BdvFunctions.show( geneData, BdvOptions.options().addTo( bdv ) ).get( 0 );
+
         source.setColor( defaultGeneColor );
-        source.setActive( true );
+        source.setDisplayRange( 0.0, dataSource.maxLutValue );
 
+        dataSource.color = defaultGeneColor;
+        dataSource.bdvSource = source;
 
-        geneSourceMap.put( gene, source );
     }
+
 
     private void addSourceFromTiffFile( String gene )
     {
-        ImagePlus imp = IJ.openImage( geneFileMap.get( gene ).toString() );
+        ImagePlus imp = IJ.openImage( dataSourcesMap.get( gene ).file.toString() );
         Img img = ImageJFunctions.wrap( imp );
 
         AffineTransform3D prosprScaling = new AffineTransform3D();
         prosprScaling.scale( PROSPR_SCALING_IN_MICROMETER );
 
-        final BdvSource source = BdvFunctions.show( img, gene,
-                Bdv.options()
-                        .addTo( bdv )
-                        .sourceTransform( prosprScaling ) );
+        final BdvSource source = BdvFunctions.show( img, gene, Bdv.options().addTo( bdv ).sourceTransform( prosprScaling ) );
 
         source.setColor( defaultGeneColor );
-        source.setActive( true );
-        geneSourceMap.put( gene, source );
-    }
 
-    private void addHideGeneButton()
-    {
-        final MutableModuleItem< Button > button = addInput( "Hide", Button.class );
-        button.setCallback( "hideGene" );
-        button.setRequired( false );
-    }
-
-    private void hideGene( )
-    {
-        final String gene = ( String ) this.getInput( GENE_SELECTION_UI );
-
-        if ( geneSourceMap.keySet().contains( gene ) )
-        {
-            geneSourceMap.get( gene ).setActive( false );
-        }
-    }
-
-
-    private void setGeneColor( ) throws SpimDataException
-    {
-        final String gene = ( String ) this.getInput( GENE_SELECTION_UI );
-
-        showGene( gene );
-
-        Color color = JColorChooser.showDialog( null,
-                "Select color for " + gene, null );
-
-        geneSourceMap.get( gene ).setColor( getArgbType( color ) );
+        dataSourcesMap.get( gene ).color = defaultGeneColor;
+        dataSourcesMap.get( gene ).bdvSource = source;
 
     }
+
 
     private ARGBType getArgbType( Color color )
     {
-        return new ARGBType(
-                    ARGBType.rgba( color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha() ) );
+        return new ARGBType( ARGBType.rgba( color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha() ) );
     }
 
-
-    private void addChangeColorButton()
+    private void initBdvWithEmRawData(  )
     {
-        final MutableModuleItem< Button > button = addInput( "Set color", Button.class );
-        button.setCallback( "setGeneColor" );
-        button.setRequired( false );
-    }
 
-    private Bdv showWithBdv( SpimData emData )
-    {
-        Bdv bdv = BdvFunctions.show( emData ).get( 0 );
+        setSourceFromBdvFile( emRawDataID );
+
+        bdv = BdvFunctions.show( dataSourcesMap.get( emRawDataID ).spimData ).get( 0 ).getBdvHandle();
+
+        bdv.getBdvHandle().getViewerPanel().setInterpolation( Interpolation.NLINEAR );
+
         //bdv.getBdvHandle().getViewerPanel().setCurrentViewerTransform( new AffineTransform3D() );
-        return bdv;
     }
 
-    private void setEmDataSimilarityTransform( )
+    private SpimData openSpimData( File file )
     {
-
-        AffineTransform3D transformJRotation = getTransformJRotation( emData );
-
-        AffineTransform3D elastixSimilarityTransform = getElastixSimilarityTransform();
-
-        AffineTransform3D combinedTransform = getCombinedTransform( transformJRotation, elastixSimilarityTransform );
-
-        adaptViewRegistration( emData, combinedTransform );
-
+        try
+        {
+            SpimData data = new XmlIoSpimData().load( file.toString() );
+            return data;
+        }
+        catch ( SpimDataException e )
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    private void adaptViewRegistration( SpimData emData, AffineTransform3D transform )
-    {
-        // the ViewRegistration in the file contains the scaling relative to 1 micrometer
-        ViewRegistration viewRegistration = emData.getViewRegistrations().getViewRegistration( 0, 0 );
-        ViewTransform viewTransform = new ViewTransformAffine( "transform",  transform );
-        viewRegistration.concatenateTransform( viewTransform );
-    }
-
-    private static AffineTransform3D getCombinedTransform( AffineTransform3D firstTransform, AffineTransform3D secondTransform )
-    {
-        AffineTransform3D combinedTransform = firstTransform.copy();
-        combinedTransform.preConcatenate( secondTransform );
-        return combinedTransform;
-    }
-
-    private AffineTransform3D getElastixSimilarityTransform()
-    {
-        AffineTransform3D elastixFixedToMoving = new AffineTransform3D();
-
-        elastixFixedToMoving.set(
-                1.16299445, -0.04662481, -0.02350539, 0.0,
-                0.05221191,  1.04386046,  0.51274918, 0.0,
-                0.00054074, -0.51328738,  1.04490107, 0.0 );
-
-        elastixFixedToMoving.translate( new double[]{ 152.92726078, -157.76850918,  466.72468048 } );
-
-        return elastixFixedToMoving.inverse();
-    }
-
-    private AffineTransform3D getTransformJRotation( SpimData emData )
-    {
-
-        AffineTransform3D transformJTransform = new AffineTransform3D(  );
-
-        transformJTransform.set(
-                -0.6427876097,   0.7660444431,   0.0,   0.0,
-                0.0, 0.0, 1.0, 0.0,
-                0.7660444431,  0.6427876097,  0.0, 0.0 );
-
-
-        FinalRealInterval boundingIntervalAfterTransformation = getBoundsAfterRotation( emData, transformJTransform );
-
-        double[] transformJTranslation = new double[]{
-                - boundingIntervalAfterTransformation.realMin( 0 ),
-                - boundingIntervalAfterTransformation.realMin( 1 ) ,
-                - boundingIntervalAfterTransformation.realMin( 2 ) };
-
-        transformJTransform.translate( transformJTranslation );
-
-        return transformJTransform;
-    }
-
-    private FinalRealInterval getBoundsAfterRotation( SpimData emData, AffineTransform3D transformJTransform )
-    {
-        RandomAccessibleInterval< ? > image = emData.getSequenceDescription().getImgLoader().getSetupImgLoader( 0 ).getImage( 0 );
-        return transformJTransform.estimateBounds( image );
-    }
-
-    private void openEmData( File directory ) throws SpimDataException
-    {
-        final File emFile = getEmFile( directory );
-        emData = new XmlIoSpimData().load( emFile.toString() );
-    }
-
-    private void createGeneSelectionUI( )
+    private void createSourceSelectionUI( )
     {
         final MutableModuleItem< String > typeItem = addInput( GENE_SELECTION_UI, String.class );
         typeItem.setPersisted( false );
         typeItem.setLabel( GENE_SELECTION_UI );
 
         List< String > genes = new ArrayList<>(  );
-        genes.addAll( geneFileMap.keySet() );
+        genes.addAll( dataSourcesMap.keySet() );
         typeItem.setChoices( genes );
     }
 
-    public void run()
+    private void initDataSources( File directory )
     {
-        // ...
-    }
 
-    private void setGeneFileMap( File directory )
-    {
+        dataSourcesMap = new TreeMap<>(  );
+
+
         File[] files = directory.listFiles();
-
-        geneFileMap = new TreeMap<>( );
 
         for ( File file : files )
         {
-            if ( file.getName().endsWith( GENE_FILE_SUFFIX )
-                    && ! file.getName().contains( EM_FILE_ID ))
-            {
-                String geneName = file.getName().replaceAll( GENE_FILE_SUFFIX, "" );
 
-                geneFileMap.put( geneName, file );
+            if ( file.getName().endsWith( DATA_SOURCE_SUFFIX ) )
+            {
+
+                String dataSourceName = file.getName().replaceAll( DATA_SOURCE_SUFFIX, "" );;
+
+                DataSource dataSource = new DataSource();
+                dataSource.file = file;
+                dataSource.maxLutValue = 255;
+
+                if ( file.getName().contains( EM_RAW_FILE_ID ) || file.getName().contains( EM_SEGMENTED_FILE_ID ) )
+                {
+                    dataSource.spimData = openSpimData( file );
+                    ProSPrRegistration.setEmSimilarityTransform( dataSource.spimData );
+
+                    if ( file.getName().contains( EM_RAW_FILE_ID ) )
+                    {
+                        emRawDataID = dataSourceName;
+                    }
+                }
+                else // prospr gene
+                {
+                    dataSource.bdvSource = null;
+                    dataSource.spimData = null;
+                }
+
+                dataSourcesMap.put( dataSourceName, dataSource );
+
+
             }
         }
 
     }
 
-    private File getEmFile( File directory )
-    {
-        File[] files = directory.listFiles();
-        ArrayList< File > emFiles = new ArrayList<>(  );
 
-        for ( File file : files )
-        {
-            if ( file.getName().endsWith( ".xml" )
-                    && file.getName().toLowerCase().contains( EM_FILE_ID )  )
-            {
-                emFiles.add( file );
-            }
-        }
-
-        if ( emFiles.size() < 1 )
-        {
-            logService.error( "No *em*.xml file found." );
-            return null;
-        }
-        else if ( emFiles.size() > 1 )
-        {
-            logService.warn(
-                    "Multiple *em*.xml files found.\n" +
-                    "Using this one: " + emFiles.get( 0 ).getName() );
-
-            return emFiles.get( 0 );
-        }
-        else
-        {
-            return emFiles.get( 0 );
-        }
-
-    }
 
     public static void main( String... args )
     {
